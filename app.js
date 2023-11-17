5ae7f175b3a0bc4b1e8630def7b1f963dd42a55a
agusjoffre <aguslj16@gmail.com>
2023-11-17 12:31:10 -0300

    added: plates,listeners fns.error w infocontainer

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const createPriceContent = (title, price) => {
 
 const menu = [];
 
-function Card(title, price, description) {
+function card(title, price, description) {
   return {
     title,
     price,
@@ -45,9 +45,54 @@ function Card(title, price, description) {
   };
 }
 
-function addMenuObject(title, price, description) {
-  const newMenu = new Card(title, price, description);
+function addMenuObject(name, price, description) {
+  const newMenu = new card(name, price, description);
   menu.push(newMenu);
-  console.log(menu);
 }
 
+let margarita = addMenuObject(
+  "Margarita",
+  "$2500",
+  "Typical Neapolitan pizza, made with San Marzano tomatoes, mozzarella cheese, fresh basil, salt, and extra-virgin olive oil."
+);
+
+let fugazzetta = addMenuObject(
+  "Fugazzetta",
+  "$3200",
+  "It is a variety of Argentine pizza, originated in Buenos Aires, which consists of placing cheese between two pizza doughs, as a filling, on top of which onion is added."
+);
+
+let jamon = addMenuObject(
+  "Ham and bell pepper",
+  "$2300",
+  "Another typical argentinian pizza, with lots of cheese"
+);
+
+let muzzarella = addMenuObject(
+  "Mozzarella",
+  "$2700",
+  "Mozzarella Pizza, made with tomato sauce, mozzarella, olives, and oregano."
+);
+
+
+function listeners() {
+  menuButton.addEventListener("click", () => {
+    createMainContent("Menu");
+    menu.forEach((obj) => {
+      createMenuContent(obj.title, obj.price, obj.description)
+    })
+  });
+
+  pricesButton.addEventListener("click", () => {
+    createMainContent("Prices");
+    menu.forEach((obj) => {
+      createPriceContent(obj.title, obj.price)
+    })
+  });
+
+  homeButton.addEventListener("click", () => {
+    createMainContent("");
+  });
+}
+
+listeners();
